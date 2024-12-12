48c0325d7a9a0e51a43524f760720e05f32bed0c
isha123321 <isha101112@hotmail.com>
2024-12-12 21:18:43 +0500

    first commit

---
+++ a3.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    pass
+print ("hello")
+
+for i in range(1,10):
+    if(i==3):
+        pass
+    print(i)
---
+++ a4.py
@@ -0,0 +1,7 @@
+def isEven(num):
+    if num % 2==0:
+        return "even"
+    else:
+     return  "odd"   
+    
+print (isEven(10))
